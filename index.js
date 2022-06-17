2c43ce03edde7e59eb32e945c72e88b7751b32d2
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-17 14:53:21 +0300

    commit tree 9b21f3e4b1dde7edb64f57064f098231bb987ff1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const words = word.length;
+  for (let i = 0; i < words / 2; i++) {
+    if (words[1] !== word[words - 1 - i]) {
+      return false;
+    }
+  }
+  return true
 }
 
 /* 
@@ -13,12 +19,20 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+console.log("Expecting:true");
+  console.log("=>", isPalindrome("abba"));
+  console.log("");
+
+  console.log("Expecting:false");
+  console.log("=>", isPalindrome("ba"));
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
-  console.log("Expecting: false");
+  console.log("Expecting: true");
   console.log("=>", isPalindrome("robot"));
 }
 
